8fb2b50d740438458c31470022b2e07fb97c1c38
PUBKABOZKAX <g_churakov@list.ru>
2024-10-27 02:38:31 +0300

    add more drinks

---
+++ src/main/java/me/udnek/toughasnailsu/command/TestCommand.java
@@ -0,0 +1,16 @@
+package me.udnek.toughasnailsu.command;
+
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class TestCommand implements @Nullable CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        commandSender.sendMessage("12121221212");
+        return true;
+    }
+}
