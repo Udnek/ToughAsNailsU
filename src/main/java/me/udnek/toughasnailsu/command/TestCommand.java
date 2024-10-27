cdd7051509df5d4edeb37e4fdd12d686a90411b8
PUBKABOZKAX <g_churakov@list.ru>
2024-10-27 23:06:49 +0300

    add customItem Tags,added all drinks except beer

--- src/main/java/me/udnek/toughasnailsu/command/TestCommand.java
+++ src/main/java/me/udnek/toughasnailsu/command/TestCommand.java
@@ -10,7 +10,10 @@ import org.jetbrains.annotations.Nullable;
 public class TestCommand implements @Nullable CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        commandSender.sendMessage("12121221212");
+        /*commandSender.sendMessage(String.valueOf(Tags.TANU_TEST.isTagged(Items.DRINKING_GLASS_BOTTLE)));
+        commandSender.sendMessage(String.valueOf(Tags.TANU_TEST));
+        commandSender.sendMessage(String.valueOf(Tags.TANU_TEST));
+        commandSender.sendMessage(String.valueOf(Tags.TANU_TEST));*/
         return true;
     }
 }
--- src/main/java/me/udnek/toughasnailsu/item/Items.java
+++ src/main/java/me/udnek/toughasnailsu/item/Items.java
@@ -4,7 +4,7 @@ import me.udnek.itemscoreu.customitem.CustomItem;
 import me.udnek.itemscoreu.customregistry.CustomRegistries;
 import me.udnek.toughasnailsu.ToughAsNailsU;
 
-public class Items {
+public class Items{
 
     public static final CustomItem  DRINKING_GLASS_BOTTLE = register(new DrinkingGlassBottle());
 
@@ -15,12 +15,16 @@ public class Items {
 
     public static final CustomItem CARROT_JUICE_BOTTLE = register(ConstructableDrinkableItem.normal("carrot_juice_bottle", 500100, 6, 1, 20*20));
     public static final CustomItem SWEET_BERRY_JUICE_BOTTLE = register(ConstructableDrinkableItem.normal("sweet_berry_juice_bottle", 500101, 6, 1, 20*20));
+    public static final CustomItem MELON_JUICE_BOTTLE = register(ConstructableDrinkableItem.normal("melon_juice_bottle", 500102, 6, 1, 20*20));
 
     public static final CustomItem AMETHYST_WATER_BOTTLE = register(ConstructableDrinkableItem.normal("amethyst_water_bottle", 500110, 6, 1, 20*20));
 
     public static final CustomItem GREEN_SWEET_BERRY_TEA_BOTTLE = register(ConstructableDrinkableItem.normal("green_sweet_berry_tea_bottle", 500200, 6, 1, 20*20));
     public static final CustomItem GREEN_GLOW_BERRY_TEA_BOTTLE = register(ConstructableDrinkableItem.normal("green_glow_berry_tea_bottle", 500201, 6, 1, 20*20));
     public static final CustomItem GREEN_SUGAR_TEA_BOTTLE = register(ConstructableDrinkableItem.normal("green_sugar_tea_bottle", 500202, 6, 1, 20*20));
+
+    public static final CustomItem MILK_CACAO_BOTTLE = register(ConstructableDrinkableItem.normal("milk_cacao_bottle", 500300, 6, 1, 20*20));
+    public static final CustomItem RAW_MILK_CACAO_BOTTLE = register(ConstructableDrinkableItem.normal("raw_milk_cacao_bottle", 500301, 6, 1, 20*20));
 
     public static CustomItem register(CustomItem customItem){
         return CustomRegistries.ITEM.register(ToughAsNailsU.getInstance(), customItem);
