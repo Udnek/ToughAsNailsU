05e6dabe1adcf63e51497482f944bc466c80a658
PUBKABOZKAX <g_churakov@list.ru>
2024-10-29 21:23:58 +0300

    right click for DRINKING_GLASS_BOTTLE

--- src/main/java/me/udnek/toughasnailsu/item/DrinkingGlassBottle.java
+++ src/main/java/me/udnek/toughasnailsu/item/DrinkingGlassBottle.java
@@ -9,7 +9,6 @@ import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -18,7 +17,6 @@ import org.bukkit.util.RayTraceResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -51,11 +49,12 @@ public class DrinkingGlassBottle extends ConstructableCustomItem implements Toug
         setComponent(new DrinkingGlassBottleComponent());
     }
 
-    public static  class DrinkingGlassBottleComponent extends RightClickableItem {
+    public static class DrinkingGlassBottleComponent extends RightClickableItem {
         private static final List<Biome> PURE_WATER_BIOMES = Arrays.asList(Biome.RIVER, Biome.FROZEN_RIVER);
         private static final List<Biome> SEA_WATER_BIOMES = Arrays.asList(Biome.OCEAN, Biome.FROZEN_OCEAN, Biome.DEEP_OCEAN, Biome.WARM_OCEAN, Biome.LUKEWARM_OCEAN, Biome.COLD_OCEAN,
                 Biome.DEEP_COLD_OCEAN, Biome.DEEP_LUKEWARM_OCEAN, Biome.DEEP_FROZEN_OCEAN);
         private static final List<Material> WATER_BLOCK = Arrays.asList(Material.WATER, Material.KELP, Material.SEAGRASS,  Material.TALL_SEAGRASS, Material.BUBBLE_COLUMN);
+
         @Override
         public void onRightClick(@NotNull CustomItem customItem, @NotNull PlayerInteractEvent event) {
             Player player = event.getPlayer();
@@ -63,7 +62,8 @@ public class DrinkingGlassBottle extends ConstructableCustomItem implements Toug
             RayTraceResult rayTraceResult = player.rayTraceBlocks(player.getAttribute(Attribute.PLAYER_BLOCK_INTERACTION_RANGE).getValue(), FluidCollisionMode.ALWAYS);
             if (rayTraceResult == null){return;}
             Block block = rayTraceResult.getHitBlock();
-            if (block == null){return;}
+            EquipmentSlot hand = event.getHand();
+            if (block == null || hand == null){return;}
             Location location = block.getLocation();
             Biome biome = location.getWorld().getBiome(location);
             if (!(block.getBlockData() instanceof Waterlogged) && !(WATER_BLOCK.contains(block.getType()))){return;}
@@ -74,10 +74,9 @@ public class DrinkingGlassBottle extends ConstructableCustomItem implements Toug
             else if (SEA_WATER_BIOMES.contains(biome)) {bottle = Items.SEA_WATER_BOTTLE.getItem();}
             else {bottle = Items.DIRTY_WATER_BOTTLE.getItem();}
 
-            inventory.removeItem(Items.DRINKING_GLASS_BOTTLE.getItem());
-            if (!(event.getHand().isHand())){inventory.setItem(event.getHand(),bottle);}
+            inventory.setItem(hand,inventory.getItem(hand).add(-1));
+            if (inventory.getItem(hand).getType() == Material.AIR){inventory.setItem(hand,bottle);}
             else {inventory.addItem(bottle);}
-
         }
     }
 }
