c5e84347df54599dd25194425da2e23540c59b9e
PUBKABOZKAX <g_churakov@list.ru>
2024-11-02 21:18:43 +0300

    add biome for seawater

--- src/main/java/me/udnek/toughasnailsu/item/DrinkingGlassBottle.java
+++ src/main/java/me/udnek/toughasnailsu/item/DrinkingGlassBottle.java
@@ -6,6 +6,7 @@ import me.udnek.itemscoreu.customitem.CustomItem;
 import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
@@ -49,7 +50,7 @@ public class DrinkingGlassBottle extends ConstructableCustomItem implements Toug
     public static class DrinkingGlassBottleComponent extends RightClickableItem {
         private static final List<Biome> PURE_WATER_BIOMES = Arrays.asList(Biome.RIVER, Biome.FROZEN_RIVER);
         private static final List<Biome> SEA_WATER_BIOMES = Arrays.asList(Biome.OCEAN, Biome.FROZEN_OCEAN, Biome.DEEP_OCEAN, Biome.WARM_OCEAN, Biome.LUKEWARM_OCEAN, Biome.COLD_OCEAN,
-                Biome.DEEP_COLD_OCEAN, Biome.DEEP_LUKEWARM_OCEAN, Biome.DEEP_FROZEN_OCEAN);
+                Biome.DEEP_COLD_OCEAN, Biome.DEEP_LUKEWARM_OCEAN, Biome.DEEP_FROZEN_OCEAN, Biome.BEACH, Biome.SNOWY_BEACH, Biome.STONY_SHORE);
         private static final List<Material> WATER_BLOCK = Arrays.asList(Material.WATER, Material.KELP, Material.SEAGRASS,  Material.TALL_SEAGRASS, Material.BUBBLE_COLUMN);
 
         @Override
