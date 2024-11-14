024358d95985be8df27a29178bf470083298780a
PUBKABOZKAX <g_churakov@list.ru>
2024-11-14 20:02:44 +0300

    add ThirstEffect

--- src/main/java/me/udnek/toughasnailsu/ToughAsNailsU.java
+++ src/main/java/me/udnek/toughasnailsu/ToughAsNailsU.java
@@ -1,6 +1,7 @@
 package me.udnek.toughasnailsu;
 
 import me.udnek.itemscoreu.customattribute.CustomAttribute;
+import me.udnek.itemscoreu.customeffect.CustomEffect;
 import me.udnek.itemscoreu.customitem.CustomItem;
 import me.udnek.itemscoreu.resourcepack.ResourcePackablePlugin;
 import me.udnek.itemscoreu.util.VanillaItemManager;
@@ -8,6 +9,7 @@ import me.udnek.toughasnailsu.attribute.Attributes;
 import me.udnek.toughasnailsu.command.DebugCommand;
 import me.udnek.toughasnailsu.data.DataTicker;
 import me.udnek.toughasnailsu.data.Database;
+import me.udnek.toughasnailsu.effect.Effects;
 import me.udnek.toughasnailsu.hud.MainHud;
 import me.udnek.toughasnailsu.item.Items;
 import me.udnek.toughasnailsu.util.EventsListener;
@@ -26,6 +28,7 @@ public final class ToughAsNailsU extends JavaPlugin implements ResourcePackableP
 
         CustomItem boot = Items.SEA_WATER_BOTTLE;
         CustomAttribute boot1 = Attributes.COLD_RESISTANCE;
+        CustomEffect boot2 = Effects.THIRST;
 
 
         VanillaItemManager.getInstance().replaceVanillaMaterial(Material.LEATHER_CHESTPLATE);
